7b5c25c138746b5e0c88045d61420cbcc62068cd
joscha <joscha@harpeng.org>
2020-01-12 16:52:39 +0100

    complete day 4

--- src/advent_of_code_2019/day04.clj
+++ src/advent_of_code_2019/day04.clj
@@ -2,14 +2,15 @@
   (:require [clojure.string :as strings]))
 
 (def double-digits ["11" "22" "33" "44" "55" "66" "77" "88" "99"])
-(def illegal-digits ["111111" "222222" "333333" "444444" "555555" "666666" "777777" "888888" "999999" "1111" "2222" "3333" "4444" "5555" "6666" "7777" "8888" "9999" "0000" "111" "222" "333" "444" "555" "666" "777" "888" "999" "000"])
+(def illegal-digits ["111111" "222222" "333333" "444444" "555555" "666666" "777777" "888888" "999999"
+                     "1111" "2222" "3333" "4444" "5555" "6666" "7777" "8888" "9999" "0000"
+                     "111" "222" "333" "444" "555" "666" "777" "888" "999" "000"])
 
 (defn find-first-double-digit [double-digits number]
-  (let [[double-digit & remaining] double-digits
-        number-string (str number)]
+  (let [[double-digit & remaining] double-digits]
     (cond
       (empty? double-digits) false
-      (strings/includes? number-string double-digit) true
+      (strings/includes? number double-digit) true
       :else (find-first-double-digit remaining number))
     )
   )
@@ -42,35 +43,42 @@
 
 (defn find-passwords-part1 [start end]
   (->> (range start end)
-      (filter twice-digit)
-      (filter growing-digits)
-      (count)
-      )
+       (map str)
+       (filter twice-digit)
+       (filter growing-digits)
+       (count)
+       )
   )
 
 (def solution-part1 (find-passwords-part1 402328 864247))
 
-(defn contains-additional-double-digit [illegal-digit number-string]
-  (twice-digit (strings/replace number-string illegal-digit "")))
-
-(defn find-first-tripple-digit [illegal-digits number]
-  (let [[tripple-digit & remaining] illegal-digits
-        number-string (str number)]
-    (cond
-      (empty? illegal-digits) false
-      (strings/includes? number-string tripple-digit) (contains-additional-double-digit tripple-digit number-string)
-      :else (find-first-tripple-digit remaining number))
+(defn remove-illegal-digits-recur [password illegal-digits]
+  (if (empty? illegal-digits)
+    password
+    (let [illegal-digit (first illegal-digits)]
+      (if (strings/includes? password illegal-digit)
+        (remove-illegal-digits-recur (strings/replace password illegal-digit "") (rest illegal-digits))
+        (remove-illegal-digits-recur password (rest illegal-digits))
+        )
+      )
     )
   )
 
-(defn tripple-digit-with-double-digit [number]
-  (find-first-tripple-digit illegal-digits number))
+(defn
+  remove-illegal-digits [passwords]
+  (for [password passwords]
+    (remove-illegal-digits-recur password illegal-digits)
+    )
+  )
 
 (defn find-passwords-part2 [start end]
   (->> (range start end)
+       (map str)
        (filter growing-digits)
+       (remove-illegal-digits)
        (filter twice-digit)
-       (filter tripple-digit-with-double-digit)
-       ;(count)
+       (count)
        )
   )
+
+(def solution-part2 (find-passwords-part2 402328 864247))
--- test/advent_of_code_2019/day04_test.clj
+++ test/advent_of_code_2019/day04_test.clj
@@ -4,19 +4,19 @@
 
 (deftest password-test
   (testing "detects numbers with two same digits"
-    (is (= (twice-digit 11246543) true))
-    (is (= (twice-digit 12335688) true))
-    (is (= (twice-digit 12345678) false))
+    (is (= (twice-digit "11246543") true))
+    (is (= (twice-digit "12335688") true))
+    (is (= (twice-digit "12345678") false))
     )
   (testing "detects numbers with three same digits"
-    (is (= (tripple-digit-with-double-digit 123444) false))
-    (is (= (tripple-digit-with-double-digit 111122) true))
-    (is (= (tripple-digit-with-double-digit 555558) false))
-    (is (= (tripple-digit-with-double-digit 777777) false))
+    (is (= (remove-illegal-digits ["123444"]) ["123"]))
+    (is (= (remove-illegal-digits ["111122"]) ["22"]))
+    (is (= (remove-illegal-digits ["555558"]) ["58"]))
+    (is (= (remove-illegal-digits ["777777"]) [""]))
     )
   (testing "detects growing digits"
-    (is (= (growing-digits 1234567) true))
-    (is (= (growing-digits 11234567) true))
-    (is (= (growing-digits 11231567) false))
+    (is (= (growing-digits "1234567") true))
+    (is (= (growing-digits "11234567") true))
+    (is (= (growing-digits "11231567") false))
     )
   )
